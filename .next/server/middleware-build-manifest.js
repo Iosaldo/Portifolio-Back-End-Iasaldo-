3558613546396d958d893356bb19440d4e7feadd
Iosaldo <iyosaldonbk@gmail.com>
2025-09-25 18:44:30 +0100

    css file

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,50 +1,23 @@
 globalThis.__BUILD_MANIFEST = {
   "pages": {
-    "/_app": [
-      "static/chunks/42529_next_dist_compiled_next-devtools_index_f41f1d7f.js",
-      "static/chunks/42529_next_dist_compiled_cce899ad._.js",
-      "static/chunks/42529_next_dist_shared_lib_b522cd3b._.js",
-      "static/chunks/42529_next_dist_client_6505cd58._.js",
-      "static/chunks/42529_next_dist_6f62f539._.js",
-      "static/chunks/42529_next_app_66a9d98e.js",
-      "static/chunks/[next]_entry_page-loader_ts_32f571e5._.js",
-      "static/chunks/42529_react-dom_bf8bdd42._.js",
-      "static/chunks/42529_0c2b1cb1._.js",
-      "static/chunks/[root-of-the-server]__9f790d11._.js",
-      "static/chunks/Desktop_React Portifolio Back-End_pages__app_2da965e7._.js",
-      "static/chunks/turbopack-Desktop_React Portifolio Back-End_pages__app_170da1b9._.js"
-    ],
-    "/_error": [
-      "static/chunks/42529_next_dist_compiled_next-devtools_index_f41f1d7f.js",
-      "static/chunks/42529_next_dist_compiled_cce899ad._.js",
-      "static/chunks/42529_next_dist_shared_lib_cc172974._.js",
-      "static/chunks/42529_next_dist_client_6505cd58._.js",
-      "static/chunks/42529_next_dist_607ca0a6._.js",
-      "static/chunks/42529_next_error_3bdd069c.js",
-      "static/chunks/[next]_entry_page-loader_ts_ca194ab0._.js",
-      "static/chunks/42529_react-dom_bf8bdd42._.js",
-      "static/chunks/42529_0c2b1cb1._.js",
-      "static/chunks/[root-of-the-server]__e38783f4._.js",
-      "static/chunks/Desktop_React Portifolio Back-End_pages__error_2da965e7._.js",
-      "static/chunks/turbopack-Desktop_React Portifolio Back-End_pages__error_3511fbe9._.js"
-    ]
+    "/_app": []
   },
   "devFiles": [],
   "ampDevFiles": [],
   "polyfillFiles": [
-    "static/chunks/42529_next_dist_build_polyfills_polyfill-nomodule.js"
+    "static/chunks/node_modules_next_dist_build_polyfills_polyfill-nomodule.js"
   ],
   "lowPriorityFiles": [],
   "rootMainFiles": [
-    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_c40c5389._.js",
-    "static/chunks/42529_next_dist_compiled_react-dom_d0a61dbf._.js",
-    "static/chunks/42529_next_dist_compiled_next-devtools_index_b0cd2d6c.js",
-    "static/chunks/42529_next_dist_compiled_ffeb49fb._.js",
-    "static/chunks/42529_next_dist_client_ab090d03._.js",
-    "static/chunks/42529_next_dist_e4d8d842._.js",
-    "static/chunks/42529_@swc_helpers_cjs_112294c8._.js",
-    "static/chunks/Desktop_React Portifolio Back-End_a0ff3932._.js",
-    "static/chunks/turbopack-Desktop_React Portifolio Back-End_abb07e30._.js"
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_57d40746._.js",
+    "static/chunks/node_modules_next_dist_compiled_react-dom_1e674e59._.js",
+    "static/chunks/node_modules_next_dist_compiled_next-devtools_index_a9cb0712.js",
+    "static/chunks/node_modules_next_dist_compiled_5150ccfd._.js",
+    "static/chunks/node_modules_next_dist_client_cf1d9188._.js",
+    "static/chunks/node_modules_next_dist_b0daae9a._.js",
+    "static/chunks/node_modules_@swc_helpers_cjs_b3dc30d6._.js",
+    "static/chunks/_a0ff3932._.js",
+    "static/chunks/turbopack-_cdba956c._.js"
   ],
   "ampFirstPages": []
 };
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,11 @@
 // src/app/layout.js
-import { Avatar, Stack } from "@mui/material";
+import { Avatar, Button, colors, Stack } from "@mui/material";
 import "./globals.css";
 import Link from "next/link";
-import * as React from 'react';
+import * as React from "react";
 
 export const metadata = {
-  title: "Meu Projeto Next.js",
+  title: "Portifolio Iasaldo",
   description: "Site de exemplo com layout global",
 };
 
@@ -14,21 +14,37 @@ export default function RootLayout({ children }) {
     <html lang="pt">
       <body>
         {/* Barra de navegação global */}
-        <header style={{ background: "#222", color: "#fff", padding: "1rem" }}>
-  <nav style={{ display: "flex", gap: "1rem" }}>
-    <Link href="/">Home</Link>
-    <Link href="/about">Sobre</Link>
-    <Link href="/contact">Contact</Link>
-    <Link href="/dashboard">Dashboard</Link>
-    <Avatar alt="Iasaldo" src="/favicon.ico" sx={{ width: 24, height: 25 }} />
-  </nav>
-</header>
+        <header style={{ padding: "1rem" }}>
+          <nav
+            style={{
+              display: "flex",
+              gap: "1rem",
+              justifyContent: "center",
+              width: "100%",
+            }}
+          >
+            <Button className="custom-button" variant="text" href="/">
+              Home
+            </Button>
+            <Button className="custom-button" variant="text" href="/dashboard">
+              Dashboard
+            </Button>
+            <Button className="custom-button" variant="text">
+              Projectos
+            </Button>
+            <Button className="custom-button" variant="text" href="/about">
+              About
+            </Button>
+          </nav>
+        </header>
 
         {/* Conteúdo das páginas */}
         <main style={{ padding: "1rem" }}>{children}</main>
 
         {/* Barra de rodapé */}
-        <footer style={{ background: "#eee", padding: "1rem", marginTop: "2rem" }}>
+        <footer
+          style={{ background: "#252422", padding: "1rem", marginTop: "2rem" }}
+        >
           <p>© 2025 - Iasaldo</p>
         </footer>
       </body>
